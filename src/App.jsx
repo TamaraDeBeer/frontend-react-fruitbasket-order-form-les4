9ff686fc1076b4b4c2b94cc07d2ca20c9dfb3569
TamaraDeBeer <tamara.debeer@hotmail.com>
2023-09-14 17:42:05 +0200

    initial css styling part 1

--- src/App.jsx
+++ src/App.jsx
@@ -59,38 +59,43 @@ return (
         <h1>Fruitmand bezorgservice</h1>
             <h2>Jouw winkelmandje:</h2>
 
-            <div>
+            <div className="fruit-container">
                 <img src={aardbei} alt="aardbei afbeelding"/>
                 <h2>Aardbeien</h2>
-                <button onClick={handleDecreaseStrawberry}>-</button>
+                <div className="fruit-button-container">
+                <button className="fruit-button" onClick={handleDecreaseStrawberry}>-</button>
                 <p>{quantityStrawberry}</p>
-                <button onClick={handleIncreaseStrawberry}>+</button>
+                <button className="fruit-button" onClick={handleIncreaseStrawberry}>+</button>
+                </div>
             </div>
 
-            <div>
+            <div className="fruit-container">
                 <img src={banaan} alt="banaan afbeelding"/>
                 <h2>Bananen</h2>
-                <button onClick={handleDecreaseBanana}>-</button>
+                <div className="fruit-button-container">
+                <button className="fruit-button" onClick={handleDecreaseBanana}>-</button>
                 <p>{quantityBanana}</p>
-                <button onClick={handleIncreaseBanana}>+</button>
-            </div>
+                <button className="fruit-button" onClick={handleIncreaseBanana}>+</button>
+            </div></div>
 
-            <div>
+            <div className="fruit-container">
                 <img src={appel} alt="appel afbeelding"/>
                 <h2>Appels</h2>
-                <button onClick={handleDecreaseApple}>-</button>
+                <div className="fruit-button-container">
+                <button className="fruit-button" onClick={handleDecreaseApple}>-</button>
                 <p>{quantityApple}</p>
-                <button onClick={handleIncreaseApple}>+</button>
-            </div>
+                <button className="fruit-button" onClick={handleIncreaseApple}>+</button>
+            </div></div>
 
-            <div>
+            <div className="fruit-container">
                 <img src={kiwi} alt="kiwi afbeelding"/>
                 <h2>Kiwis</h2>
-                <button onClick={handleDecreaseKiwi}>-</button>
+                <div className="fruit-button-container">
+                <button className="fruit-button" onClick={handleDecreaseKiwi}>-</button>
                 <p>{quantityKiwi}</p>
-                <button onClick={handleIncreaseKiwi}>+</button>
-            </div>
-            <button type="button" onClick={resetForm}>Reset</button>
+                <button className="fruit-button" onClick={handleIncreaseKiwi}>+</button>
+            </div></div>
+            <button className="reset-button" type="button" onClick={resetForm}>Reset</button>
 
             </main>
     </>
